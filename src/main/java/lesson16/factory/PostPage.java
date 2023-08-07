0a428e0558b5e1874f7c5f70f4930346253065fa
AnnaBudinova <anbori@abv.bg>
2023-08-07 09:36:52 +0300

    rename java classs

--- src/main/java/lesson16/factory/PostPage.java
+++ src/main/java/lesson16/factory/PostPage.java
@@ -1,6 +1,5 @@
 package lesson16.factory;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,19 +9,19 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class NewPostPage {
+public class PostPage {
     public static final String PAGE_URL = "http://training.skillo-bg.com:4300/posts/create";
     private final WebDriver driver;
     @FindBy(xpath = "//*[@class='text-center']")
     private WebElement isTextDisplayed;
-    public NewPostPage(WebDriver driver) {
+    public PostPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
     public boolean isUrlLoaded() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        return wait.until(ExpectedConditions.urlContains(NewPostPage.PAGE_URL));
+        return wait.until(ExpectedConditions.urlContains(PostPage.PAGE_URL));
     }
     public String isTextDisplayed() {
         return isTextDisplayed.getText();
